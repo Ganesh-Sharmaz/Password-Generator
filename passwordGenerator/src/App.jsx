d391a9ec5fa281c6faff49b3df1114aa7de403e1
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-01-26 23:38:17 +0530

    update

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -6,17 +6,17 @@ function App() {
 
     <>
     <div className='flex justify-center items-center w-full h-screen ' >
-      <div className=' bg-gray-600 rounded-xl px-3 py-2 shadow-lg mt-2'>
-        <h1 className='px-4 text-lg text-white text-center'>Password Generator</h1>
-        <div className='flex items-center justify-center px-3 py-3'>
+      <div className=' bg-gray-600 rounded-xl px-3 py-2 shadow-lg mt-2 max-w-md mx-auto '>
+        <h1 className='px-4 text-lg text-white text-center mb-2'>Password Generator</h1>
+        <div className='flex shadow-lg rounded-xl overflow-hidden mb-4'>
           <input 
           type='text'
           // value={Password}
-          className='w-full px-3 py-1 rounded-md shadow-md '
+          className='w-full px-3 py-1'
           readOnly
           placeholder='Password'
           />
-          <button className='outline-none  text-white  focus:bg-opacity-5 px-3 py-0.5 bg-blue-900 rounded-md'>copy</button>
+          <button className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'>copy</button>
         </div>
       </div>
 
