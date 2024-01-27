eb413fc04576ce511ee9c398aa76ea3a13d32ae3
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-01-27 13:22:57 +0530

    Range is working

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 function App() {
+  
+  const [Password, setPassword] = useState("");
+  const [Length, setLength] = useState(8);
+  const [NumbersAllowed, setNumbersAllowed] = useState(false);
+  const [CharactersAllowed, setCharactersAllowed] = useState(false);
+
+  const passwordGenerator = useCallback(
+    () => {
+      let pass = ''
+      let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+      if (NumbersAllowed) str += "0123456789";
+      if (CharactersAllowed) str += "~!@#$%^&*()_+"
+
+      for(i=1; i<=Length; i++){
+        let char = Math.floor(Math.random() * str.length+1 );
+        pass += str.charAt(char)
+      }
+
+      setPassword(pass)
+
+    },[Length, CharactersAllowed,NumbersAllowed, setPassword]
+  )
+
+
+
 
   return (
 
     <>
-      <div className='flex justify-center items-center w-full h-screen ' >
-        <div className=' bg-gray-600 rounded-xl px-3 py-2 shadow-lg mt-2 max-w-md mx-auto mb-4 '>
+      <div className=' overflow-hidden flex justify-center items-center w-full h-screen ' >
+        <div className=' bg-gray-500 rounded-xl px-3 py-2 shadow-lg mt-2 mb-4 w-{600} '>
 
           {/* Password Input and Copy starts here */}
 
@@ -14,7 +40,7 @@ function App() {
           <div className='flex shadow-lg rounded-xl overflow-hidden mb-4'>
             <input 
             type='text'
-            // value={Password}
+            value={Password}  
             className='w-full px-3 py-1'
             readOnly
             placeholder='Password'
@@ -30,10 +56,11 @@ function App() {
               type='range'
               min={8}
               max={100}
-              className='rounded-full cursor-pointer'
-              // value={Length}
+              className='rounded-full shadow-sm cursor-pointer'
+              value={Length}
+              onChange={(e) => {setLength(e.target.value)}}
               />
-              <label className="text-white ">Length:</label>
+              <label className="text-white ">Length: {Length}</label>
             </div>
 
             <div className='flex items-center text-center gap-x-1'>
